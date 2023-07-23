9547ce30d8f74fd6af874c8921518a203309c75e
Aashutosh <aashutoshchouhan2@gmail.com>
2023-07-23 16:22:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let closeLogin = document.getElementById("closelogin");
+let login = document.getElementById("login");
+let loginRegisterBtn = document.getElementById("loginRegisterBtn");
+loginRegisterBtn.addEventListener("click", () => {
+  login.style.display = "block";
+});
+closeLogin.addEventListener("click", () => {
+  login.style.display = "none";
+});
+
+const success = () => {
+  swal("Congratulations!", "You are logged in", "success");
+  setTimeout(() => {
+    login.style.display = "none";
+  }, 2000);
+};
